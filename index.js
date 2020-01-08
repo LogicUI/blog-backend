604b5df262f260d8254a778b3fec8bec2eed120a
john <john.wee@flex-solver.com.sg>
2020-01-08 22:36:17 +0800

    john remove mail route from index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const knex = require("./db");
 const cors = require("cors");
 const express = require("express");
 const authRoute = require("./routes/auth-route");
-const mailRoute = require("./routes/mail");
 const app = express();
 const PORT = 3000;
 
@@ -10,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth",authRoute);
-app.use("/email",mailRoute);
+
 
 const verifyToken = ((req,res,next) => {
     const bearerHeader = req.headers["authorization"];
