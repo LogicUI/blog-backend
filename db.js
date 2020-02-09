9afd09f789620b8579e147c0cb739ba59673e721
john <john.wee@flex-solver.com.sg>
2020-02-09 20:30:01 +0800

    update db to sqllite

--- db.js
+++ db.js
@@ -1,28 +1,34 @@
+
+const mysql = require("mysql");
+const sqllite3 = require("sqlite3").verbose();
 require("dotenv").config();
-const knex = require("knex")({
-    client:"mysql",
-    connection: {
+
+let connection;
+if (process.env.NODE_ENV === "local") {
+    connection = mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database:process.env.DB_DATABASE
-    }
-})
+        database: process.env.DB_DATABASE
+    })
 
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE
-// })
+    connection.connect(function (err) {
+        if (err) {
+            return console.error("error" + err.message);
+        }
+        console.log("Connected to MySQL server");
+    })
 
-// connection.connect(function(err){
-//     if(err) {
-//         return console.error("error" + err.message);
-//     }
-//     console.log("Connected to MySQL server");
-// })
+} else if (process.env.NODE_ENV === "test") {
+    connection = new sqllite3.Database(":memory:", (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("Connected to the in-memory sqllite database")
+    });
+}
 
 
 
-module.exports = knex;
+
+module.exports = connection;
